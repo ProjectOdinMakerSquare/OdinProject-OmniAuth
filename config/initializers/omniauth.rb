b133295674d98b054fa7c8d7786bac665a65e3f4
Tim Blair <tim@bla.ir>
2013-02-19 19:38:21 +0000

    Add ability to specify GitHub auth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  provider :github,
+    ENV['OMNIAUTH_PROVIDER_KEY'],
+    ENV['OMNIAUTH_PROVIDER_SECRET'],
+    :scope => ENV['GITHUB_SCOPE']
 end
